6d97fbc884216a7597702768110e8f627324eabe
kingz <1477027382@qq.com>
2023-12-18 17:29:23 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,14 +1,14 @@
-<template>
-  <div>
-    我是首页
-    <!-- 二级路由出口 -->
-    <RouterView></RouterView>
-  </div>
-</template>
-
-<script>
-export default {};
+<script setup>
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
 </script>
 
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
 <style>
 </style>
