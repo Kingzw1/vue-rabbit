c32bf8928d15451ebb76164c4d986488bc048264
kingz <1477027382@qq.com>
2023-12-18 22:57:44 +0800

    新鲜好物和人气推荐props/插槽的使用

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,3 +6,25 @@ export const getBannerAPI = () => {
         url: '/home/banner'
     })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+/**
+ * @description: 人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const HotNewAPI = () => {
+    return httpInstance({
+        url: '/home/hot'
+    })
+}
