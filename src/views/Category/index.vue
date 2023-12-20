7dc2c1e7f7f2abd8077275ae4d43176176cc6c1e
kingz <1477027382@qq.com>
2023-12-20 17:19:18 +0800

    激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,12 +3,13 @@ import { getCategoryAPI } from "@/apis/category";
 import { getBannerAPI } from "@/apis/home";
 import { onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
+import GoodsItem from "../Home/components/GoodsItem.vue";
 const route = useRoute();
 // 获取数据
-const catgoryData = ref({});
+const categoryData = ref({});
 const getCategory = async () => {
   const res = await getCategoryAPI(route.params.id);
-  catgoryData.value = res.data.result;
+  categoryData.value = res.data.result;
 };
 // onMounted(() => {
 //   getCategory();
@@ -38,7 +39,7 @@ onMounted(() => {
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{ catgoryData.name }}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 轮播图 -->
@@ -48,6 +49,25 @@ onMounted(() => {
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+    </div>
+    <div class="sub-list">
+      <h3>全部分类</h3>
+      <ul>
+        <li v-for="i in categoryData.children" :key="i.id">
+          <RouterLink to="/">
+            <img :src="i.picture" />
+            <p>{{ i.name }}</p>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
+    <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+      <div class="head">
+        <h3>- {{ item.name }}-</h3>
+      </div>
+      <div class="body">
+        <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
       </div>
     </div>
   </div>
@@ -60,6 +80,7 @@ onMounted(() => {
   height: 500px;
   left: 0;
   top: 0;
+  margin: 0 auto;
 
   img {
     width: 100%;
@@ -83,6 +104,7 @@ onMounted(() => {
       display: flex;
       padding: 0 32px;
       flex-wrap: wrap;
+      justify-content: center;
 
       li {
         width: 168px;
