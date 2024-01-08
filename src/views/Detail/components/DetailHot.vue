d179cd3ab9b49bba22ff76386400e376d6ab5f70
kingz <1477027382@qq.com>
2024-01-08 21:03:17 +0800

    详情热榜-title和列表内容的适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,8 +1,20 @@
 <script setup>
 // 以24小时热榜获取数据渲染模板
 import { fetchHotGoodsAPI } from "@/apis/detail.js";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+// 设计props参数 适配不同的title和数据
+const props = defineProps({
+  hotType: {
+    type: Number,
+  },
+});
+// 适配title 1-24小时热榜 2-周热榜
+const TYPEMAP = {
+  1: "24小时热榜",
+  2: "周热榜",
+};
+const title = computed(() => TYPEMAP[props.hotType]);
 // 1.封装接口
 // 2.调用接口渲染模板
 const hotList = ref([]);
@@ -10,7 +22,7 @@ const route = useRoute();
 const getHotList = async () => {
   const res = await fetchHotGoodsAPI({
     id: route.params.id,
-    type: 1,
+    type: props.hotType,
   });
   hotList.value = res.data.result;
 };
@@ -22,7 +34,7 @@ onMounted(() => {
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink
       to="/"
