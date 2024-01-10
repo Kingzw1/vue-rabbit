993a124618c0eec28809b3802bed6166c6fee586
kingz <1477027382@qq.com>
2024-01-10 14:56:47 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -7,6 +7,7 @@ import { ref } from "vue";
 const form = ref({
   account: "",
   password: "",
+  agree: true,
 });
 // 2.准备表单验证规则对象
 const rules = {
@@ -14,6 +15,20 @@ const rules = {
   password: [
     { required: true, message: "请输入密码", trigger: "blur" },
     { min: 6, max: 14, message: "长度在 6 到 14 个字符", trigger: "blur" },
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        // console.log(value);
+        // 自定义校验逻辑
+        // 勾选就通过 不勾选就不通过
+        if (value) {
+          callback();
+        } else {
+          callback(new Error("请勾选同意用户协议"));
+        }
+      },
+    },
   ],
 };
 </script>
@@ -53,8 +68,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
