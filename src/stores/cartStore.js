d2a17dbc9bee7e401ed11cdc6b9e9dd478ce8702
kingz <1477027382@qq.com>
2024-01-12 21:23:31 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -28,6 +28,13 @@ export const useCartStore = defineStore('cart', ()=>{
         cartList.value.splice(idx,1)
         }
 
+    // 单选功能
+    const singleCheck = (skuId,selected) => {
+        // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+       const item= cartList.value.find(item => item.skuId === skuId)
+        item.selected=selected
+        }
+
     // 计算属性
     // 1.总的数量 所有项的count之和
     const allCount=computed(()=>cartList.value.reduce((p,c)=>p+c.count,0))
@@ -39,7 +46,8 @@ export const useCartStore = defineStore('cart', ()=>{
         addCart,
         delCart,
         allCount,
-        allPrice
+        allPrice,
+        singleCheck
     }
 },{
     persist: true,
