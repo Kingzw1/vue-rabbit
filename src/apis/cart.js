20f905ee81482cb623efee48768f7f3c14a2d994
kingz <1477027382@qq.com>
2024-01-13 15:37:19 +0800

    接口购物车-删除功能&小优化

--- src/apis/cart.js
+++ src/apis/cart.js
@@ -1,21 +1,33 @@
 // 封装购物车相关接口
-import httpInstance from "@/utils/http"
+import httpInstance from "@/utils/http";
 
 // 加入购物车
-export const insertCartAPI =({skuId,count})=>{
-    return httpInstance({
-        url:'/member/cart',
-        method:'post',
-        data:{
-            skuId,
-            count
-        }
-    })
-}
+export const insertCartAPI = ({ skuId, count }) => {
+  return httpInstance({
+    url: "/member/cart",
+    method: "post",
+    data: {
+      skuId,
+      count,
+    },
+  });
+};
 
-export const findCartListAPI = ()=>{
-    return httpInstance({
-        url:'/member/cart',
-        method:'get'
-    })
-}
+// 过去最新的购物车列表
+export const findCartListAPI = () => {
+  return httpInstance({
+    url: "/member/cart",
+    method: "get",
+  });
+};
+
+// 删除购物车
+export const delCartAPI = (ids) => {
+  return httpInstance({
+    url: "/member/cart",
+    method: "delete",
+    data: {
+      ids,
+    },
+  });
+};
