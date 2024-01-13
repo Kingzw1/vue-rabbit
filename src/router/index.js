73e02fa0dc697b1ff8725faef8e3f02e9a70751d
kingz <1477027382@qq.com>
2024-01-13 22:57:37 +0800

    结算页-路由配置以及基础数据的渲染

--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,58 @@
 // createRouter:创建router实例对象
 // createWebHistory:创建history模式路由
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // path和component对应关系的位置
   routes: [
     {
-      path: '/',
+      path: "/",
       // name: 'home',
-      component: () => import('../views/Layout/index.vue'),
+      component: () => import("../views/Layout/index.vue"),
       children: [
         {
-          path: '',
-          name: 'home',
-          component: () => import('../views/Home/index.vue')
+          path: "",
+          name: "home",
+          component: () => import("../views/Home/index.vue"),
         },
         {
-          path: 'category/:id',
-          name: 'category',
-          component: () => import('../views/Category/index.vue')
+          path: "category/:id",
+          name: "category",
+          component: () => import("../views/Category/index.vue"),
         },
         {
-          path: 'category/sub/:id',
-          name: 'subcategory',
-          component: () => import('../views/SubCategory/index.vue')
+          path: "category/sub/:id",
+          name: "subcategory",
+          component: () => import("../views/SubCategory/index.vue"),
         },
         {
-          path: 'detail/:id',
-          name: 'detail',
-          component: () => import('../views/Detail/index.vue')
+          path: "detail/:id",
+          name: "detail",
+          component: () => import("../views/Detail/index.vue"),
         },
         {
-          path: 'cartList',
-          name: 'cartList',
-          component: () => import('../views/CartList/index.vue')
-        }
-      ]
+          path: "cartList",
+          name: "cartList",
+          component: () => import("../views/CartList/index.vue"),
+        },
+        {
+          path: "checkout",
+          name: "checkout",
+          component: () => import("../views/Checkout/index.vue"),
+        },
+      ],
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/Login/index.vue')
-    }
+      path: "/login",
+      name: "login",
+      component: () => import("../views/Login/index.vue"),
+    },
   ],
   // 路由滚动行为定制
   scrollBehavior() {
-    return { top: 0 }
-  }
-})
+    return { top: 0 };
+  },
+});
 
-export default router
+export default router;
