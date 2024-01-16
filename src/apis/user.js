6abffc788e7e4e548b28942b00f07fb681d92ec3
kingz <1477027382@qq.com>
2024-01-16 14:52:30 +0800

    会员中心-个人信息数据渲染

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,12 +1,21 @@
 // 封装所有的和用户相关的接口函数
-import httpInstance from "@/utils/http"
-export const loginAPI =({account,password})=>{
-    return httpInstance({
-        url:'/login',
-        method:'POST',
-        data:{
-            account,
-            password
-        }   
-    })
-}
+import httpInstance from "@/utils/http";
+export const loginAPI = ({ account, password }) => {
+  return httpInstance({
+    url: "/login",
+    method: "POST",
+    data: {
+      account,
+      password,
+    },
+  });
+};
+
+export const getLikeListAPI = ({ limit = 4 }) => {
+  return httpInstance({
+    url: "/goods/relevant",
+    params: {
+      limit,
+    },
+  });
+};
